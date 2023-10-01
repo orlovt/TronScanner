22fa8fce2107312655ccbd37c83d07025696d11b
Timothy Orlov <orlovt7@yandex.ru>
2023-10-01 16:03:15 -0500

    upd

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"net/http"
+	"os"
 )
 
 // address:TJ7m1yk5fbhhWkhxiM6Jh5ZH8zrSTvbi6D		2023-01-01
+
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", serveSankey)
 	http.HandleFunc("/getdata", handler)
-	http.ListenAndServe(":8080", nil)
+
+	// Get the PORT from the environment variable
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not specified
+	}
+
+	// Start the server using the specified port
+	http.ListenAndServe(":"+port, nil)
 }
